df665caca59d36dbbd630a32cf75b78353c06640
Steve Tolzmann <steven.tolzmann@gmail.com>
2021-06-06 22:08:01 -0500

    Implemented header

---
+++ source/Header.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FileEncryptor
+{
+    internal static class Header
+    {
+        public static byte[] GetBytes()
+        {
+            return new byte[] { 0xFF, (byte)Version.CurrentVersion }; // 2 Bytes
+        }
+    }
+
+    public enum Version : byte
+    {
+        CurrentVersion = 0x01 // V1
+    }
+}
